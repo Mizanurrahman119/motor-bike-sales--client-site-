327e144faa40fca5830acb070545e82e84b7ba12
Mizanurrahman119 <niloy719119@gmail.com>
2021-11-15 23:01:03 +0600

    message

--- src/Pages/Login/Firebase/Firebase.config.js
+++ src/Pages/Login/Firebase/Firebase.config.js
@@ -6,12 +6,4 @@ const firebaseConfig = {
     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
     appId: process.env.REACT_APP_APP_ID,
   };
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCk80b2Lh8E8Yj9t5rzbrOTU7sGPKuA4Ng",
-//   authDomain: "bike-sales-center.firebaseapp.com",
-//   projectId: "bike-sales-center",
-//   storageBucket: "bike-sales-center.appspot.com",
-//   messagingSenderId: "897148353715",
-//   appId: "1:897148353715:web:02aa4d5b0aa53b24f444c6"
-// };
   export default firebaseConfig;
